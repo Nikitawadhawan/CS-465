5cb300ad563d4feb675165819d8eb910874b2726
Nikitawadhawan <nikita.wadhawan@snhu.edu>
2023-11-27 01:29:02 +0300

    Create new application API

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,24 +1,23 @@
 const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1'
-let dbURI = `mongodb://${host}/travlr`;
+const dbURL = `mongodb://${host}/travlr`;
 const readLine = require('readline');
 
-//avoid 'current Server Discovery and Monitoring engine is deprecated'
-// mongoose.set('useUnifiedTopology', true);
+if (process.env.NODE_ENV === 'production') {
+  dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
+}
 
 const connect = () => {
-    setTimeout(() => mongoose.connect(dbURI, {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    }), 1000);
+    setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, createIndexes: true, useUnifiedTopology: true }), 1000);
 }
+
 
 mongoose.connection.on('connected', () => {
   console.log('connected');
 });
 
 mongoose.connection.on('error', err => {
-  console.log('Cnnection error:' + err);
+  console.log('error: ' + err);
   return connect();
 });
 
@@ -27,13 +26,13 @@ mongoose.connection.on('disconnected', () => {
 });
 
 if (process.platform === 'win32') {
-    const rl = readLine.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    rl.on('SIGINT', () => {
-        process.emit("SIGINT");
-    });
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ('SIGINT', () => {
+    process.emit("SIGINT");
+  });
 }
 
 const gracefulShutdown = (msg, callback) => {
@@ -43,23 +42,22 @@ const gracefulShutdown = (msg, callback) => {
   });
 };
 
-// For nodemon restarts                                 
 process.once('SIGUSR2', () => {
   gracefulShutdown('nodemon restart', () => {
     process.kill(process.pid, 'SIGUSR2');
   });
 });
-// For app termination
 process.on('SIGINT', () => {
   gracefulShutdown('app termination', () => {
     process.exit(0);
   });
 });
-// For Heroku app termination
 process.on('SIGTERM', () => {
   gracefulShutdown('Heroku app shutdown', () => {
     process.exit(0);
   });
 });
+
+connect();
 
 require('./travlr');
