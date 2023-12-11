9d4193896036aef57266b8008ae2da7857508f7c
Nikitawadhawan <nikita.wadhawan@snhu.edu>
2023-12-11 11:50:10 +0300

    added default message for homepage if not logged in

--- app.js
+++ app.js
@@ -17,11 +17,6 @@ require('./app_api/config/passport');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
-// var aboutRouter = require('./app_server/routes/about');
-// var contactRouter = require('./app_server/routes/contact');
-// var mealsRouter = require('./app_server/routes/meals');
-// var newsRouter = require('./app_server/routes/news');
-// var roomsRouter = require('./app_server/routes/rooms');
 const apiRouter = require('./app_api/routes/index');
 
 var app = express();
@@ -52,11 +47,6 @@ app.use('/api', (req, res, next) => {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
-// app.use('/about', aboutRouter);
-// app.use('/contact', contactRouter);
-// app.use('/meals', mealsRouter);
-// app.use('/news', newsRouter);
-// app.use('/rooms', roomsRouter);
 app.use('/api', apiRouter);
 
 // catch unauthorized error and create 401
